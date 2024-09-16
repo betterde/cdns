b2d95fa275c46f8c0314bdbe4794831d4d4a53ad
George King <george@betterde.com>
2024-09-16 09:19:58 +0800

    minor: optimize project config

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"errors"
 	"github.com/betterde/cdns/internal/journal"
 	"github.com/spf13/viper"
 	"os"
@@ -13,70 +14,68 @@ const TLSModeFile = "file"
 var Conf *Config
 
 type Config struct {
-	NS        NS        `yaml:"ns"`
-	SOA       SOA       `yaml:"soa"`
-	DNS       DNS       `yaml:"dns"`
-	HTTP      HTTP      `yaml:"http"`
-	Ingress   Ingress   `yaml:"ingress"`
-	Logging   Logging   `yaml:"logging"`
-	Providers Providers `yaml:"providers"`
+	NS        NS        `yaml:"ns" mapstructure:"NS"`
+	SOA       SOA       `yaml:"soa" mapstructure:"SOA"`
+	DNS       DNS       `yaml:"dns" mapstructure:"DNS"`
+	HTTP      HTTP      `yaml:"http" mapstructure:"HTTP"`
+	Ingress   Ingress   `yaml:"ingress" mapstructure:"INGRESS"`
+	Logging   Logging   `yaml:"logging" mapstructure:"LOGGING"`
+	Providers Providers `yaml:"providers" mapstructure:"PROVIDERS"`
 }
 
 type NS struct {
-	IP string `yaml:"ip"`
+	IP string `yaml:"ip" mapstructure:"IP"`
 }
 
 type Logging struct {
-	Level string `yaml:"level"`
+	Level string `yaml:"level" mapstructure:"LEVEL"`
 }
 
 type DNS struct {
-	Admin    string            `yaml:"admin"`
-	Listen   string            `yaml:"listen"`
-	NSName   string            `yaml:"nsname"`
-	Records  map[string]Record `yaml:"records"`
-	Protocol string            `yaml:"protocol"`
+	Admin    string            `yaml:"admin" mapstructure:"ADMIN"`
+	Listen   string            `yaml:"listen" mapstructure:"LISTEN"`
+	NSName   string            `yaml:"nsname" mapstructure:"NSNAME"`
+	Records  map[string]Record `yaml:"records" mapstructure:"RECORDS"`
+	Protocol string            `yaml:"protocol" mapstructure:"PROTOCOL"`
 }
 
 type HTTP struct {
-	TLS      TLS    `yaml:"tls"`
-	Domain   string `yaml:"domain"`
-	Listen   string `yaml:"listen"`
-	Username string `yaml:"username"`
-	Password string `yaml:"password"`
+	TLS    TLS    `yaml:"tls" mapstructure:"TLS"`
+	Domain string `yaml:"domain" mapstructure:"DOMAIN"`
+	Listen string `yaml:"listen" mapstructure:"LISTEN"`
 }
 
 type Record struct {
-	Type  string `yaml:"type"`
-	Value string `yaml:"value"`
+	Type  string `yaml:"type" mapstructure:"TYPE"`
+	Value string `yaml:"value" mapstructure:"VALUE"`
 }
 
 type Ingress struct {
-	IP string `yaml:"ip"`
+	IP string `yaml:"ip" mapstructure:"IP"`
 }
 
 type Providers struct {
-	ACME ACME `yaml:"acme"`
-	File File `yaml:"file"`
+	ACME ACME `yaml:"acme" mapstructure:"ACME"`
+	File File `yaml:"file" mapstructure:"FILE"`
 }
 
 type TLS struct {
-	Mode string `yaml:"mode"`
+	Mode string `yaml:"mode" mapstructure:"MODE"`
 }
 
 type SOA struct {
-	Domain string `yaml:"domain"`
+	Domain string `yaml:"domain" mapstructure:"DOMAIN"`
 }
 
 type ACME struct {
-	Email   string `yaml:"email"`
-	Server  string `yaml:"server"`
-	Storage string `yaml:"storage"`
+	Email   string `yaml:"email" mapstructure:"EMAIL"`
+	Server  string `yaml:"server" mapstructure:"SERVER"`
+	Storage string `yaml:"storage" mapstructure:"STORAGE"`
 }
 
 type File struct {
-	TLSKey  string `yaml:"tlsKey"`
-	TLSCert string `yaml:"tlsCert"`
+	TLSKey  string `yaml:"tlsKey" mapstructure:"TLSKEY"`
+	TLSCert string `yaml:"tlsCert" mapstructure:"TLSCERT"`
 }
 
 func Parse(file string) {
@@ -84,6 +83,7 @@ func Parse(file string) {
 		viper.SetConfigFile(file)
 	} else {
 		viper.AddConfigPath(".")
+		viper.SetConfigType("yaml")
 		viper.SetConfigName(".cdns")
 		viper.AddConfigPath("/etc/cdns")
 	}
@@ -92,10 +92,94 @@ func Parse(file string) {
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	viper.SetEnvPrefix("CDNS")
 
+	var notFoundError viper.ConfigFileNotFoundError
+
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err != nil {
-		journal.Logger.Sugar().Errorf("Failed to read configuration file: %s", err)
-		os.Exit(1)
+	if err := viper.ReadInConfig(); err != nil && errors.As(err, &notFoundError) {
+		viper.SetDefault("DNS.LISTEN", "0.0.0.0:53")
+		viper.SetDefault("DNS.PROTOCOL", "both")
+		viper.SetDefault("HTTP.LISTEN", "0.0.0.0:443")
+		viper.SetDefault("LOGGING.LEVEL", "DEBUG")
+
+		err = viper.BindEnv("NS.IP", "CDNS_NS_IP")
+		if err != nil {
+			journal.Logger.Sugar().Error(err)
+		}
+
+		err = viper.BindEnv("DNS.ADMIN", "CDNS_DNS_ADMIN")
+		if err != nil {
+			journal.Logger.Sugar().Error(err)
+		}
+
+		err = viper.BindEnv("DNS.NSNAME", "CDNS_DNS_NSNAME")
+		if err != nil {
+			journal.Logger.Sugar().Error(err)
+		}
+
+		err = viper.BindEnv("DNS.LISTEN", "CDNS_DNS_LISTEN")
+		if err != nil {
+			journal.Logger.Sugar().Error(err)
+		}
+
+		err = viper.BindEnv("DNS.PROTOCOL", "CDNS_DNS_PROTOCOL")
+		if err != nil {
+			journal.Logger.Sugar().Error(err)
+		}
+
+		err = viper.BindEnv("SOA.DOMAIN", "CDNS_SOA_DOMAIN")
+		if err != nil {
+			journal.Logger.Sugar().Error(err)
+		}
+
+		err = viper.BindEnv("HTTP.TLS.MODE", "CDNS_HTTP_TLS_MODE")
+		if err != nil {
+			journal.Logger.Sugar().Error(err)
+		}
+
+		err = viper.BindEnv("HTTP.DOMAIN", "CDNS_HTTP_DOMAIN")
+		if err != nil {
+			journal.Logger.Sugar().Error(err)
+		}
+
+		err = viper.BindEnv("HTTP.LISTEN", "CDNS_HTTP_LISTEN")
+		if err != nil {
+			journal.Logger.Sugar().Error(err)
+		}
+
+		err = viper.BindEnv("INGRESS.IP", "CDNS_INGRESS_IP")
+		if err != nil {
+			journal.Logger.Sugar().Error(err)
+		}
+
+		err = viper.BindEnv("LOGGING.LEVEL", "CDNS_LOGGING_LEVEL")
+		if err != nil {
+			journal.Logger.Sugar().Error(err)
+		}
+
+		err = viper.BindEnv("PROVIDERS.ACME.EMAIL", "CDNS_PROVIDERS_ACME_EMAIL")
+		if err != nil {
+			journal.Logger.Sugar().Error(err)
+		}
+
+		err = viper.BindEnv("PROVIDERS.ACME.SERVER", "CDNS_PROVIDERS_ACME_SERVER")
+		if err != nil {
+			journal.Logger.Sugar().Error(err)
+		}
+
+		err = viper.BindEnv("PROVIDERS.ACME.STORAGE", "CDNS_PROVIDERS_ACME_STORAGE")
+		if err != nil {
+			journal.Logger.Sugar().Error(err)
+		}
+
+		err = viper.BindEnv("PROVIDERS.FILE.TLSKEY", "CDNS_PROVIDERS_FILE_TLSKEY")
+		if err != nil {
+			journal.Logger.Sugar().Error(err)
+		}
+
+		err = viper.BindEnv("PROVIDERS.FILE.TLSCERT", "CDNS_PROVIDERS_FILE_TLSCERT")
+		if err != nil {
+			journal.Logger.Sugar().Error(err)
+		}
 	}
 
 	// read in environment variables that match
